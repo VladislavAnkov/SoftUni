42fd1dad498093f0a259702ce0142c38150d9fa2
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-13 20:13:11 +0200

    Add solution
    Upload Stacks-And-Queues cs projects.

---
+++ C#-Advanced/01.StacksAndQueues/Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string[] input = Console.ReadLine().Split();
+
+			Stack<string> reversed = new Stack<string>(input.Reverse());
+
+			while (reversed.Count > 1)
+			{
+				int firstNum = int.Parse(reversed.Pop());
+				string mathOperator = reversed.Pop();
+				int secondNum = int.Parse(reversed.Pop());
+				int result = 0;
+
+				if (mathOperator == "+")
+				{
+					result = firstNum + secondNum;
+				}
+				else
+				{
+					result = firstNum - secondNum;
+				}
+
+				reversed.Push(result.ToString());
+
+			}
+
+			Console.WriteLine(int.Parse(reversed.Pop()));
+		}
+    }
+}
