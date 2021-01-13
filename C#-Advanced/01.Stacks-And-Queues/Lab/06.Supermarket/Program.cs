a47f957967fefb37b711f018cc1d66c2fae68933
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-13 20:05:05 +0200

    Create C# Advanced folder

---
+++ C#-Advanced/01.Stacks-And-Queues/Lab/06.Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string input = Console.ReadLine();
+
+			Queue<string> clients = new Queue<string>();
+
+			while (input != "End")
+			{
+				if (input != "Paid")
+				{
+					clients.Enqueue(input);
+				}
+				else
+				{
+					int n = clients.Count;
+					for (int i = 0; i < n; i++)
+					{
+						Console.WriteLine(clients.Dequeue());
+					}
+				}
+
+				input = Console.ReadLine();
+			}
+
+			Console.WriteLine("{0} people remaining.", clients.Count);
+		}
+    }
+}
