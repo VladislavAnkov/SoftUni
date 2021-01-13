8fcce7ecff0bf341a3038a644ced51bb30077caf
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-13 19:31:18 +0200

    Add solution
    Add Stacks and Queues Lab solutions

---
+++ C# Advanced/01.Stacks-And-Queues/Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] chArray = input.ToCharArray();
+
+            Stack<char> result = new Stack<char>();
+
+            for (int i = 0; i < chArray.Length; i++)
+            {
+                result.Push(chArray[i]);
+            }
+
+            for (int j = 0; j < chArray.Length; j++)
+            {
+                Console.Write(result.Pop());
+            }
+        }
+    }
+}
