95f883b6dce40ed14a69ce24d3a2cf86ece71c55
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-16 11:33:55 +0200

    Add new csproj to repo

---
+++ C#-Advanced/01.StacksAndQueues/Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int numberOfCars = int.Parse(Console.ReadLine());
+
+			Queue<string> cars = new Queue<string>();
+
+			string command = Console.ReadLine();
+
+			int carsPassed = 0;
+
+			while (command != "end")
+			{
+				if (command == "green")
+				{
+					for (int i = 0; i < numberOfCars; i++)
+					{
+						if (cars.Count > 0)
+						{
+							Console.WriteLine($"{cars.Dequeue()} passed!");
+							carsPassed++;
+						}
+						else
+						{
+							break;
+						}
+					}
+				}
+				else
+				{
+					cars.Enqueue(command);
+				}
+
+				command = Console.ReadLine();
+			}
+
+			Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+		}
+    }
+}
