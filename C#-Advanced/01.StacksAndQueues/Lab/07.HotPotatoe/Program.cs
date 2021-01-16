2e600a89a56f37eb4c01e68a009cfb3a79207d83
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-16 11:26:46 +0200

    Update Stacks-And-Queues sln

---
+++ C#-Advanced/01.StacksAndQueues/Lab/07.HotPotatoe/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotatoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string[] input = Console.ReadLine().Split();
+
+			Queue<string> kids = new Queue<string>(input);
+
+			int n = int.Parse(Console.ReadLine());
+
+			int count = 1;
+
+			while (kids.Count > 1)
+			{
+				if (count == n)
+				{
+					Console.WriteLine("Removed " + kids.Dequeue());
+					count = 1;
+				}
+				else
+				{
+					kids.Enqueue(kids.Dequeue());
+					count++;
+				}
+			}
+
+			Console.WriteLine("Last is " + kids.Dequeue());
+		}
+    }
+}
