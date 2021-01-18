1b10df1c8fddf8da3cac5de06289b1b4bdc6f660
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-18 20:04:38 +0200

    Add cs.proj
    Added Multidimensional arrays Lab

---
+++ C#-Advanced/02.Multidimensional-Arrays/05.SquareWithMaxSum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SquareWithMaxSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+			int rows = matrixSize[0];
+			int cols = matrixSize[1];
+
+			int[,] matrix = new int[rows, cols];
+			for (int row = 0; row < rows; row++)
+			{
+				int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+				for (int col = 0; col < cols; col++)
+				{
+					matrix[row, col] = input[col];
+				}
+				Console.WriteLine();
+			}
+
+			int maxSum = int.MinValue;
+			int rowIndex = -1;
+			int colIndex = -1;
+
+			for (int r = 0; r < rows - 1; r++)
+			{
+				for (int c = 0; c < cols - 1; c++)
+				{
+					int temp = matrix[r, c] + matrix[r, c + 1] + matrix[r + 1, c] + matrix[r + 1, c + 1];
+
+					if (temp > maxSum)
+					{
+						maxSum = temp;
+						rowIndex = r;
+						colIndex = c;
+					}
+				}
+			}
+
+			Console.WriteLine($"{matrix[rowIndex, colIndex]} {matrix[rowIndex, colIndex + 1]}");
+			Console.WriteLine($"{matrix[rowIndex + 1, colIndex]} {matrix[rowIndex + 1, colIndex + 1]}");
+			Console.WriteLine(maxSum);
+		}
+    }
+}
---
+++ C#-Advanced/02.Multidimensional-Arrays/06.JaggedArray-Mod/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.JaggedArray_Mod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			int[][] jaggedArr = new int[n][];
+
+			for (int row = 0; row < jaggedArr.Length; row++)
+			{
+				int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+				jaggedArr[row] = new int[input.Length];
+				for (int col = 0; col < input.Length; col++)
+				{
+					jaggedArr[row][col] = input[col];
+				}
+				Console.WriteLine();
+			}
+
+			string[] cmdArgs = Console.ReadLine().Split();
+
+			while (cmdArgs[0] != "END")
+			{
+				string command = cmdArgs[0];
+				int row = int.Parse(cmdArgs[1]);
+				int col = int.Parse(cmdArgs[2]);
+				int @value = int.Parse(cmdArgs[3]);
+
+				if (row < 0 || row >= jaggedArr.Length ||
+					col < 0 || col >= jaggedArr.Length)
+				{
+					Console.WriteLine("Invalid coordinates");
+					cmdArgs = Console.ReadLine().Split();
+					continue;
+				}
+
+				if (command == "Add")
+				{
+					jaggedArr[row][col] += @value;
+				}
+				else if (command == "Subtract")
+				{
+					jaggedArr[row][col] -= @value;
+				}
+
+				cmdArgs = Console.ReadLine().Split();
+			}
+
+			for (int r = 0; r < jaggedArr.Length; r++)
+			{
+				for (int c = 0; c < jaggedArr[r].Length; c++)
+				{
+					Console.Write(jaggedArr[r][c] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
