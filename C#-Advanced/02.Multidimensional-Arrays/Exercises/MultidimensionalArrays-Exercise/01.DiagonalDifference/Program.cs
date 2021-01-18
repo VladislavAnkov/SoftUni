1f70ef74bb83e196508e27860102c539d177cb43
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-18 21:10:28 +0200

    Add .csproj

---
+++ C#-Advanced/02.Multidimensional-Arrays/Exercises/MultidimensionalArrays-Exercise/01.DiagonalDifference/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int size = int.Parse(Console.ReadLine());
+
+			int[,] matrix = new int[size, size];
+
+			int primarySum = 0;
+			int secondarySum = 0;
+
+			for (int row = 0; row < matrix.GetLength(0); row++)
+			{
+				int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+				for (int col = 0; col < matrix.GetLength(1); col++)
+				{
+					matrix[row, col] = input[col];
+				}
+				Console.WriteLine();
+			}
+
+			for (int row = 0; row < size; row++)
+			{
+				int col = row;
+				primarySum += matrix[row, col];
+			}
+
+			int currentCol = 0;
+
+			for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+			{
+				secondarySum += matrix[row, currentCol];
+				currentCol++;
+			}
+
+			int difference = primarySum - secondarySum;
+			Console.WriteLine(Math.Abs(difference));
+		}
+    }
+}
---
+++ C#-Advanced/02.Multidimensional-Arrays/Exercises/MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			int rows = dimensions[0];
+			int cols = dimensions[1];
+
+			string[,] matrix = new string[rows, cols];
+
+			for (int row = 0; row < matrix.GetLength(0); row++)
+			{
+				string[] input = Console.ReadLine().Split();
+				for (int col = 0; col < matrix.GetLength(1); col++)
+				{
+					matrix[row, col] = input[col];
+				}
+				Console.WriteLine();
+			}
+
+			int count = 0;
+
+			for (int row = 0; row < rows - 1; row++)
+			{
+				for (int col = 0; col < cols - 1; col++)
+				{
+					if (matrix[row, col] == matrix[row, col + 1] &&
+					   matrix[row, col] == matrix[row + 1, col] &&
+					   matrix[row, col] == matrix[row + 1, col + 1])
+					{
+						count++;
+					}
+				}
+			}
+
+			Console.WriteLine(count);
+		}
+    }
+}
