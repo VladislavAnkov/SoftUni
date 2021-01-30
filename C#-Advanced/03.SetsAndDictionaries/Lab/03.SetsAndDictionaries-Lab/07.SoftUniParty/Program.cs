4acd480b6cb820cf34d9747c5bfd5d51aa6f3a88
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-30 11:23:08 +0200

    Add lab solution

---
+++ C#-Advanced/03.SetsAndDictionaries/Lab/03.SetsAndDictionaries-Lab/07.SoftUniParty/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            string guest = Console.ReadLine(); ;
+            bool isPartyStarted = false;
+
+            while (guest  != "END")
+            {
+                if (Char.IsNumber(guest, 0))
+                {
+                    vipGuests.Add(guest);
+                }
+                else if (guest.Length == 8)
+                {
+                    regularGuests.Add(guest);
+                }
+
+                if (guest == "PARTY")
+                {
+                    isPartyStarted = true;
+
+                    while (guest != "END")
+                    {
+                        guest = Console.ReadLine();
+
+                        if (vipGuests.Contains(guest))
+                        {
+                            vipGuests.Remove(guest);
+                        }
+                        else if (regularGuests.Contains(guest))
+                        {
+                            regularGuests.Remove(guest);
+                        }
+                    }
+                }
+
+                if (isPartyStarted)
+                {
+                    break;
+                }
+
+                guest = Console.ReadLine();
+            }
+
+            int didNotCome = vipGuests.Count + regularGuests.Count;
+
+            Console.WriteLine(didNotCome) ;
+
+            foreach (string item in vipGuests)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (string item in regularGuests)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
