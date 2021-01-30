5e0f88c6d75ff8a04ef504fed88109adb83ae261
VladislavAnkov <76256778+VladislavAnkov@users.noreply.github.com>
2021-01-30 11:29:37 +0200

    Add lab solution

---
+++ C#-Advanced/04.FunctionalProgramming/Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			char[] delimiters = new char[] { ' ', ',' };
+
+			List<int> numbers = Console.ReadLine()
+				.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => Convert.ToInt32(x))
+				.Where(x => x % 2 == 0)
+				.OrderBy(x => x)
+				.ToList();
+
+
+			Console.WriteLine(string.Join(", ", numbers));
+		}
+    }
+}
